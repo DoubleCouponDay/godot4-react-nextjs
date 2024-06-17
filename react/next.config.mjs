2f919cbc2bbe067324d71561265829b845d3caee
Doublecouponday <mail@samueljenks.me>
2024-06-17 20:04:43 +1200

    fix: not serving in development

--- react/next.config.mjs
+++ react/next.config.mjs
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    reactStrictMode: false,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md', 'glsl', 'vert', 'frag'],
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+            test: /\.glsl$/,
+            use: 'raw-loader',
+        });
+
+        // Add Three.js file extensions to the list of watched files
+        if (!isServer) {
+            config.resolve.extensions.push('.glsl', '.vert', '.frag');
+        }
+
+        return config;
+    },
+    async headers() { //allow godot4 to be served in development
+    return [
+        {
+        source: '/(.*)',
+        headers: [
+            {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+            },
+            {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+            },
+        ],
+        },
+    ];
+    },
 };
 
 export default nextConfig;
